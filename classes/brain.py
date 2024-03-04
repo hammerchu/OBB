8975d94bac7b447e4f582a159028430fd44b0620
hammerchu <hammerchu@gmail.com>
2024-03-04 14:38:45 +0800

    ready to change structure to class

--- classes/brain.py
+++ classes/brain.py
@@ -4,15 +4,13 @@ import logging
 from threading import Thread
 import time
 import asyncio
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from flask_socketio import SocketIO
-from navigate import Navigate
-import navigate
 
-from classes.navigate import Navigate
-from classes.plan import Plan
-from classes.call import Call
+from bot.classes.navigate import Navigate
+from bot.classes.plan import Plan
+from bot.classes.call import Call
 
 logging.basicConfig(level=logging.INFO)
 
@@ -33,40 +31,55 @@ app.config.from_object('config')
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 CORS(app)
 
-STATUS_CHECK_FREQ = 10 # check status 10 times one min
+STATUS_CHECK_FREQ = 20 
 
 nav = asyncio.run(Navigate._init_()) # Nav instance
 plan = Plan() # Plan instance
 call = Call('M', 60, 0) # Call and Streaming instance
 call.run('https://onbotbot.daily.co/_test')
 
-def ask_for_help():
+def track_nav():
+    '''
+    Running in a separate thread to check if bbot stuck
+    '''
     is_asking_help = False
     while True:
+        call.heart_beat() # Send heart beat
+
         if nav.is_navigating:
-            logging.info('Nav code report: ', nav.nav_code)
-            if nav.nav_code and nav.nav_code >= 6:
-                # ask for help from HUBS
-                call.send_message(f'HELP - nav.nav_code: {nav.nav_code}')
+            logging.debug('Nav code: ', nav.nav_code)
+            if not is_asking_help and nav.nav_code and nav.nav_code >= 6:
+                '''ask for help from HUBS'''
+                logging.info('Stuck: ', nav.nav_code)
+                call.stuck_message(nav.nav_code)
                 is_asking_help = True
+            elif is_asking_help and nav.nav_code and nav.nav_code < 6:
+                '''recovered from stuck'''
+                logging.info('Recovered from stuck: ', nav.nav_code)
+                call.recovered_message(nav.nav_code)
+                is_asking_help = False
+        else:
+            pass
+
         time.sleep(60/STATUS_CHECK_FREQ)
 
 
-
 @app.route('/goto', methods=['POST'])
-def goto():
+def travel_to():
     '''
     Travel to target station
     '''
-    print('/post_goto')
+    dest_station_id = request.get_json()['dest_station_id']
+    logging.info(f'Start travel to dest_station_id : { dest_station_id} ')
+    return ""
+    
 
 
 def bringup():
     '''
-    Prepare all the necessary components
+    Do all the bringup task here
     '''
-    #do all the bringup task here
-    pass
+    Thread(target=track_nav).run()
     
 
 
@@ -74,6 +87,6 @@ def bringup():
 
 if __name__ == '__main__':
     logging.info('\n\n          -----BRAIN-----\n\n')
-    
+    bringup()
     app.run(host='0.0.0.0', port=8000, debug=True)
 
